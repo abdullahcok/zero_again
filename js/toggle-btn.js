c7d4ebf84d0d41d0713a00b44483f0fa159e9443
Abdullah Çok <abdullahcok@yandex.com>
2020-12-28 08:07:11 +0300

    another ones added

--- js/toggle-btn.js
+++
@@ -1,7 +0,0 @@
-let body= document.body;
-let toggleBtn = document.querySelector('.toggle-btn');
-let currentTheme = localStorage.getItem('currentTheme');
-
-toggleBtn.addEventListener('click', function(){
-  body.classList.toggle(dark-theme);
-});
