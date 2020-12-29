352dab3860f31b2bfd93a4f5cf28c3268ce946be
Abdullah Çok <abdullahcok@yandex.com>
2020-12-30 02:54:49 +0300

    walking steady

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let socialNav = document.querySelector('.social-m');
+let socialToggler = document.querySelector('.s-toggler');
+
+socialToggler.addEventListener('click', function(){
+  socialNav.classList.toggle('active');
+});
